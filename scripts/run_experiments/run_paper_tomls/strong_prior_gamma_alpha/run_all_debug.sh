4f6dd01fe222b4d9fbb494f1361b677c74ce161b
bearecinos <beatriz.recinos@ed.ac.uk>
2025-02-19 11:49:10 +0000

    changes to tomls to run in bow

--- scripts/run_experiments/run_paper_tomls/strong_prior_gamma_alpha/run_all_debug.sh
+++ scripts/run_experiments/run_paper_tomls/strong_prior_gamma_alpha/run_all_debug.sh
@@ -46,7 +46,7 @@ echo $OUT | mail -s "run fwd finish config" beatriz.recinos@ed.ac.uk
 #mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_errorprop.py $2 |& tee $path_logs/log_toml_errprop.txt
 #OUT=$(tail "$path_logs/log_toml_errprop.txt")
 #echo $OUT | mail -s "run errp finish config" beatriz.recinos@ed.ac.uk
-#
+
 #echo $(date -u) "Run inv_sigma stages started" | mail -s "run inv sigma started" beatriz.recinos@ed.ac.uk
 #mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_invsigma.py $2 |& tee $path_logs/log_toml_invsig.txt
 #OUT=$(tail "$path_logs/log_toml_invsig.txt")
