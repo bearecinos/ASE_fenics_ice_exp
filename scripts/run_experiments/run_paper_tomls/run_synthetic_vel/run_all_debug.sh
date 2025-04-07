4fc2441d3ead274f0461ffb15aa28d2df4e5504e
bearecinos <beatriz.recinos@ed.ac.uk>
2025-04-07 11:21:36 +0100

    latest synthetic configurations

--- scripts/run_experiments/run_paper_tomls/run_synthetic_vel/run_all_debug.sh
+++ scripts/run_experiments/run_paper_tomls/run_synthetic_vel/run_all_debug.sh
@@ -27,9 +27,9 @@ toml set --toml-path $2 io.input_dir "$input_run_inv"
 toml set --toml-path $2 io.output_dir "$run_inv_output_dir/output"
 toml set --toml-path $2 io.diagnostics_dir "$run_inv_output_dir/diagnostics"
 
-#toml set --toml-path $3 io.input_dir "$input_run_inv"
-#toml set --toml-path $3 io.output_dir "$run_inv_output_dir/output"
-#toml set --toml-path $3 io.diagnostics_dir "$run_inv_output_dir/diagnostics"
+toml set --toml-path $3 io.input_dir "$input_run_inv"
+toml set --toml-path $3 io.output_dir "$run_inv_output_dir/output"
+toml set --toml-path $3 io.diagnostics_dir "$run_inv_output_dir/diagnostics"
 
 #echo $(date -u) "Run inversion stages started" | mail -s "run inv started" beatriz.recinos@ed.ac.uk
 #mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_inv.py $2 |& tee $path_logs/log_toml_inv_two.txt
@@ -41,7 +41,7 @@ mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_forward.py $2 |& tee $path_log
 OUT=$(tail "$path_logs/log_toml_fwd_two.txt")
 echo $OUT | mail -s "run fwd finish config 2" beatriz.recinos@ed.ac.uk
 
-#echo $(date -u) "Run forward stages started" | mail -s "run fwd started" beatriz.recinos@ed.ac.uk
-#mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_forward.py $3 |& tee $path_logs/log_toml_fwd_three.txt
-#OUT=$(tail "$path_logs/log_toml_fwd_three.txt")
-#echo $OUT | mail -s "run fwd finish config 3" beatriz.recinos@ed.ac.uk
+echo $(date -u) "Run forward stages started" | mail -s "run fwd started" beatriz.recinos@ed.ac.uk
+mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_forward.py $3 |& tee $path_logs/log_toml_fwd_three.txt
+OUT=$(tail "$path_logs/log_toml_fwd_three.txt")
+echo $OUT | mail -s "run fwd finish config 3" beatriz.recinos@ed.ac.uk
